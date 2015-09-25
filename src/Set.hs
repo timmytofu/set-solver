ae04b5ee1aeecb1f1fb4e33f125e56d70be1e76a
timmy_tofu <timmytofu@users.noreply.github.com>
2015-09-24 21:57:32 -0400

    Deals until there's at least one set or the deck is empty

--- src/Set.hs
+++ src/Set.hs
@@ -142,33 +142,38 @@ findSets' (Board bs cs) = let pairs = filter notSame $ (,) <$> cs <*> cs
 deal' :: Game' Board
 deal' = do
     Game deck board <- get
-    let (cards, newDeck) = dealFor board deck
-        newBoard = boardFrom cards
+    let (newBoard, newDeck) = dealFor board deck
     put $ Game newDeck newBoard
     return newBoard
 
--- | Deals until the board has twelve cards, if there's enough in the deck,
--- or if the board already has twelve or more cards adds three more if there's
--- enough in the deck.
--- TODO: just deal until we know there's a set
--- >>> let (cs, d) = dealFor (Board undefined (map toEnum [0..11])) (map toEnum [12..14])
+-- | Deals until there are at least 12 cards (if there's enough in the
+-- deck), continuing if there's no set in the new board
+-- >>> let (Board _ cs, d) = dealFor (Board [] (map toEnum [0..11])) (map toEnum [12..14])
 -- >>> map fromEnum cs
 -- [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 -- >>> d
 -- []
--- >>> let (cs, d) = dealFor (Board undefined (map toEnum [0..5])) (map toEnum [6..14])
+-- >>> let (Board _ cs, d) = dealFor (Board [] (map toEnum [0..5])) (map toEnum [6..14])
 -- >>> map fromEnum cs
 -- [0,1,2,3,4,5,6,7,8,9,10,11]
 -- >>> map fromEnum d
 -- [12,13,14]
-dealFor :: Board -> Deck -> ([Card], Deck)
+-- >>> let (Board _ cs, d) = dealFor (Board [] (map toEnum [1,10,11,20,21,31,33,40,41,50,51,52])) (map toEnum [54,55,73,74])
+-- >>> length cs
+-- 16
+-- >>> d
+-- []
+dealFor :: Board -> Deck -> (Board, Deck)
 dealFor (Board _ cards) deck =
     let count = if length cards >= 12
                   then 3
                   else 12 - length cards
         (newCards, deckRem) = splitAt (min count (length deck)) deck
+        newBoard = boardFrom $ cards ++ newCards
     in
-    (cards ++ newCards, deckRem)
+    if null (findSets newBoard) && not (null deck)
+      then dealFor newBoard deckRem
+      else (newBoard, deckRem)
 
 -- | Removes a set from the current game board if those cards are all in play
 --   Otherwise, no-op
