3cea8b802831bd0469f88017a8a60124c4b61550
timmy_tofu <timmytofu@users.noreply.github.com>
2015-09-28 10:12:03 -0400

    Adds Arbitrary instance for Board, uses it to check findSet[']

--- spec/SetSpec.hs
+++ spec/SetSpec.hs
@@ -16,12 +16,18 @@ instance Arbitrary Card where
       where minCard = fromEnum (minBound :: Card)
             maxCard = fromEnum (maxBound :: Card)
 
+instance Arbitrary Board where
+    arbitrary =
+      boardFrom . map toEnum . take 12
+        <$> shuffle [minCard .. maxCard]
+      where minCard = fromEnum (minBound :: Card)
+            maxCard = fromEnum (maxBound :: Card)
+
+
 spec :: Spec
 spec = do
     describe "two methods" $ do
-      prop "findSet and findSet' find the same sets" $ \cs ->
-        let board = boardFrom (uniq cs) -- TODO: avoid uniq with Arbitrary instance for Board
-        in
+      prop "findSet and findSet' find the same sets" $ \board ->
         findSets board == findSets' board
     describe "card enum isomorphism" $ do
       prop "Card and Int isomorphic through fromEnum and toEnum" $ \c ->
